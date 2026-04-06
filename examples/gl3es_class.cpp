319ec839577f240a44e2259f0e1029b072527be6
Cordell Barron <cordell.barron@gmail.com>
2026-04-06 23:41:11 +0100

    Added gles3 examples not working.

---
+++ examples/gl3es_class.cpp
@@ -0,0 +1,201 @@
+
+#include "../include/shogl.hpp"
+#include <cmath>
+
+class myWindow : public shogl_window
+{
+    GLuint vao_;
+    GLuint program_;
+    unsigned int pointBuffer_, colourBuffer_;
+
+    // Rotation angle...
+    float angle_ = 0;
+
+    GLuint rotationMatrixLocation_;
+    float rotationMatrix_[16];
+
+    GLFN(GLGENVERTEXARRAYS, glGenVertexArrays)
+    GLFN(GLGENBUFFERS, glGenBuffers)
+    GLFN(GLBINDBUFFER, glBindBuffer)
+    GLFN(GLBUFFERDATA, glBufferData)
+    GLFN(GLENABLEVERTEXATTRIBARRAY, glEnableVertexAttribArray)
+    GLFN(GLDISABLEVERTEXATTRIBARRAY, glDisableVertexAttribArray)
+    GLFN(GLVERTEXATTRIBPOINTER, glVertexAttribPointer)
+    GLFN(GLUSEPROGRAM, glUseProgram)
+    GLFN(GLBINDVERTEXARRAY, glBindVertexArray)
+    GLFN(GLGETUNIFORMLOCATION, glGetUniformLocation)
+    GLFN(GLUNIFORMMATRIX4FV, glUniformMatrix4fv)
+    GLFN(GLDELETEPROGRAM, glDeleteProgram)
+    GLFN(GLDELETEVERTEXARRAYS, glDeleteVertexArrays)
+    GLFN(GLDELETEBUFFERS, glDeleteBuffers)
+
+    GLuint glsl_compile(GLuint type, const std::string& source)
+    {
+        GLFN(GLCREATESHADER, glCreateShader)
+            GLFN(GLSHADERSOURCE, glShaderSource)
+            GLFN(GLCOMPILESHADER, glCompileShader)
+            GLFN(GLLINKPROGRAM, glLinkProgram)
+            GLFN(GLGETSHADERIV, glGetShaderiv)
+            GLFN(GLGETSHADERINFOLOG, glGetShaderInfoLog)
+
+            GLuint shaderID = glCreateShader(type);
+        const char* src = source.c_str();
+
+        glShaderSource(shaderID, 1, &src, NULL);
+        glCompileShader(shaderID);
+
+        GLint result = GL_FALSE;
+        glGetShaderiv(shaderID, GL_COMPILE_STATUS, &result);
+        if (result == GL_FALSE)
+        {
+            int infoLogLength;
+            glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &infoLogLength);
+            std::vector<GLchar> error(infoLogLength);
+            glGetShaderInfoLog(shaderID, infoLogLength, &infoLogLength, &error[0]);
+            throw std::runtime_error(std::string(&error[0], error.size()));
+        }
+        return shaderID;
+    }
+
+    GLuint glsl_link(const std::vector<GLuint>& shaders)
+    {
+        GLFN(GLCREATEPROGRAM, glCreateProgram)
+            GLFN(GLATTACHSHADER, glAttachShader)
+            GLFN(GLDETACHSHADER, glDetachShader)
+            GLFN(GLLINKPROGRAM, glLinkProgram)
+            GLFN(GLGETPROGRAMIV, glGetProgramiv)
+            GLFN(GLGETPROGRAMINFOLOG, glGetProgramInfoLog)
+
+            GLuint programID = glCreateProgram();
+        for (unsigned int s = 0; s < shaders.size(); ++s)
+            glAttachShader(programID, shaders[s]);
+
+        glLinkProgram(programID);
+        GLint result = GL_FALSE;
+        glGetProgramiv(programID, GL_LINK_STATUS, &result);
+        if (result == GL_FALSE)
+        {
+            int InfoLogLength;
+            glGetProgramiv(programID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+            std::vector<GLchar> error(InfoLogLength);
+            glGetProgramInfoLog(programID, InfoLogLength, &InfoLogLength, &error[0]);
+            throw std::runtime_error(std::string(&error[0], error.size()));
+        }
+
+        for (unsigned int s = 0; s < shaders.size(); ++s)
+            glDetachShader(programID, shaders[s]);
+
+        return programID;
+    }
+
+public:
+    myWindow()
+    {
+        // Create our geometry...
+        std::vector<float> points = { -0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f };
+        std::vector<float> colours = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
+
+        glGenVertexArrays(1, &vao_);
+        glBindVertexArray(vao_);
+
+        glGenBuffers(1, &pointBuffer_);
+        glBindBuffer(GL_ARRAY_BUFFER, pointBuffer_);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * points.size(), &points.front(), GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, NULL);
+
+        glGenBuffers(1, &colourBuffer_);
+        glBindBuffer(GL_ARRAY_BUFFER, colourBuffer_);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * colours.size(), &colours.front(), GL_STATIC_DRAW);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, NULL);
+
+        glBindVertexArray(NULL);
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+
+        GLuint vertexShader = glsl_compile(GL_VERTEX_SHADER, R"(
+            #version 300 es
+            layout(location=0) in vec3 inPoint;
+            layout(location=1) in vec3 inColour;
+            out vec3 colour;
+            uniform mat4 rotationMatrix;
+            void main()
+            {
+                    gl_Position = rotationMatrix * vec4(inPoint, 1.0);
+                colour = inColour;
+            }
+        )");
+
+        GLuint fragmentShader = glsl_compile(GL_FRAGMENT_SHADER, R"(
+            #version 300 es
+            in vec3 colour;
+            out vec4 frag;
+            void main()
+            {
+                frag = vec4(colour, 1.0);
+            }
+        )");
+
+        program_ = glsl_link({ vertexShader, fragmentShader });
+        rotationMatrixLocation_ = glGetUniformLocation(program_, "rotationMatrix");
+
+        glUseProgram(NULL);
+    }
+
+    void draw()
+    {
+        angle_ += 0.1f;
+        rotationMatrix_[0] = static_cast<float>(cos(angle_));
+        rotationMatrix_[1] = static_cast<float>(sin(angle_));
+        rotationMatrix_[4] = static_cast<float>(-sin(angle_));
+        rotationMatrix_[5] = static_cast<float>(cos(angle_));
+        rotationMatrix_[10] = 1.0f;
+        rotationMatrix_[15] = 1.0f;
+
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(program_);
+        glUniformMatrix4fv(rotationMatrixLocation_, 1, GL_FALSE, rotationMatrix_);
+        glBindVertexArray(vao_);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        glBindVertexArray(NULL);
+        glUseProgram(NULL);
+    }
+
+    void resize(int w, int h)
+    {
+        glViewport(0, 0, w, h);
+    }
+
+    void kill()
+    {
+        glDeleteBuffers(1, &pointBuffer_);
+        glDeleteBuffers(1, &colourBuffer_);
+        glDeleteVertexArrays(1, &vao_);
+        glDeleteProgram(program_);
+    }
+
+    void key_down(int x, int y, unsigned int key)
+    {
+        if (key == 27)
+            shogl()->window_quit(0);
+        else
+            shogl()->window_event_behaviour(shogl()->window_event_behaviour() == shogl_window::peekEvent ?
+                shogl_window::waitForEvent : shogl_window::peekEvent);
+    }
+};
+
+SHOGL_CLASS(myWindow)
+{
+    shogl()->window_title("Hello triangle (class) GL3ES");
+    shogl()->window_fps(60);
+    shogl()->window_size(600, 600);
+
+    return shogl()->window_show();
+}
--- makefile
+++ makefile
@@ -3,11 +3,11 @@ LDLIBS = -lGL -lX11
 
 $(shell mkdir bin)
 
-all : gl1_class gl1_main gl4_class gl4_main
+all : gl1_class gl1_main gl4_class gl4_main gl3es_main gl3es_class
 	
 gl1_class :
 	$(CXX) examples/gl1_class.cpp -o bin/gl1_class $(LDLIBS)
-
+	
 gl1_main :
 	$(CXX) examples/gl1_main.cpp -o bin/gl1_main $(LDLIBS)
 
@@ -16,4 +16,10 @@ gl4_class :
 
 gl4_main :
 	$(CXX) examples/gl4_main.cpp -o bin/gl4_main $(LDLIBS)
+
+gl3es_class :
+	$(CXX) examples/gl3es_class.cpp -o bin/gl3es_class $(LDLIBS)
+
+gl3es_main :
+	$(CXX) examples/gl3es_main.cpp -o bin/gl3es_main $(LDLIBS)
 
